7e3196b31e1af5a769a98f5bb7fc4aabe46a4cbb
DmytriyKryvosheyev <7612997@gmail.com>
2017-05-27 17:05:55 +0300

    getmyfriends

---
+++ getmyfriends/index.js
@@ -0,0 +1,28 @@
+const aws = require('./../lib/aws');
+const getuser = require('./../lib/get-user.js');
+
+const getMyFriends = (data) => {
+    return getuser(data.authorizer.principalId).then(user=>{
+        var friends = [];
+        for (var i = 0, len = user.contacts.length; i < len; i++) {
+            var friend  = getuser(user.contacts[i]);
+            friends.push(friend);
+        }
+        return Promise.all(friends);
+    }).then( allfriends => {
+        return allfriends
+            .filter(friend => friend !== undefined)
+            .map((friend, key) => {
+            return {
+                id: friend.id,
+                name: friend.name,
+                status: friend.status,
+                profession: friend.profession,
+                avatar: friend.avatar
+            };
+        });
+    })
+
+}
+
+module.exports = getMyFriends;
