29ee295631a8b07fb623cadf20af3f1c0bb34b6b
Mark Orel <mail.ormark@gmail.com>
2017-05-28 07:38:37 +0300

    added users locations to friends response

--- friendsgetall/index.js
+++ friendsgetall/index.js
@@ -1,6 +1,12 @@
 const aws = require('./../lib/aws');
 const getuser = require('./../lib/get-user.js');
 
+const getLocation = () => {
+    return aws.dynamodb.scan({
+      TableName: 'Locations'
+    }).promise().then(res => res.Items);
+}
+
 const FriendsGetAll = (data) => {
     return getuser(data.authorizer.principalId).then(user=>{
         var friends = [];
@@ -8,17 +14,24 @@ const FriendsGetAll = (data) => {
             var friend  = getuser(user.contacts[i]);
             friends.push(friend);
         }
-        return Promise.all(friends);
-    }).then(allfriends => {
+        return Promise.all(friends).then(res => {
+            return getLocation().then(l => {
+                return [res, l];
+            });
+        });
+    }).then(res => {
+        const [allfriends, locations] = res;
         return allfriends
             .filter(friend => friend !== undefined)
             .map((friend, key) => {
+            const l = locations.find(l => l.user_id === friend.id);
             return {
                 id: friend.id,
                 name: friend.name,
                 status: friend.status,
                 profession: friend.profession,
-                avatar: friend.avatar
+                avatar: friend.avatar,
+                location: l ? l.location : null,
             };
         });
     })
